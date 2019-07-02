08c1cba99df8bf5f362b968692a5d4703ebe6de9
bradeac <bradeacamil@gmail.com>
2019-07-03 01:24:23 +0300

    Add explaining comments to the example

--- example/src/App.js
+++ example/src/App.js
@@ -2,15 +2,17 @@ import React from 'react'
 import useGapi from 'usegapi'
 
 const App = () => {
-  const response = useGapi('https://www.googleapis.com/auth/youtube.readonly')
+    // using the readonly youtube scope for this example
+    // response will either contain the user data or an error object
 
-  console.log(response)
+    const response = useGapi('https://www.googleapis.com/auth/youtube.readonly')
 
-  return (
-    <div>
-      <div id='google-signin-button'/>
-    </div>
-  )
+    return (
+        <div>
+            // mandatory div. use this exact same id !!!
+            <div id='google-signin-button' />
+        </div>
+    )
 }
 
 export default App
